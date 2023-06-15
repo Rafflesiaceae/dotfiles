ae312158a72348ed8dd9339b9e47f23844b1fed3
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2023-06-16 01:35:40 +0200

    Add git-reset-file

--- list.py
+++ list.py
@@ -105,6 +105,7 @@ sources = {
     "./.local/bin/git-list-changed-files-in-commit": "",
     "./.local/bin/git-rebase-noninteractive": "",
     "./.local/bin/git-remote-copy-url": "",
+    "./.local/bin/git-reset-file": "",
     "./.local/bin/git-reset-submodule": "",
     "./.local/bin/git-reset-to-tracked": "",
     "./.local/bin/git-search-all-branches": "",
