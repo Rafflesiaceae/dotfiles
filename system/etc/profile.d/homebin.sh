757889f1b76c103f859013183e0a254a889e4315
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2025-07-18 09:39:43 +0200

    system: homebin ArchLinux upstream clean

--- system/etc/profile.d/homebin.sh
+++ system/etc/profile.d/homebin.sh
@@ -1,3 +1,1 @@
-# Do not change this unless you want to completely by-pass Arch Linux' way
-# of handling Java versions and vendors. Instead, please use script `archlinux-java`
 export PATH=$PATH:$HOME/.local/bin
