a8b80d6da0d4177585ef51771c810777a1277f32
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2025-08-25 00:59:04 +0200

    ranger: Add autoflat plug

--- list.py
+++ list.py
@@ -32,6 +32,7 @@ sources = {
     "./.config/nvim/init.vim": "",
     "./.config/nvim/lua/treesitter-nu.lua": "",
     "./.config/parcellite/parcelliterc": {"linux": ""},
+    "./.config/ranger/plugins/auto_flat_jvm_projects.py": {"linux": ""},
     "./.config/ranger/rc.conf": {"linux": ""},
     "./.config/ranger/rifle.conf": {"linux": ""},
     "./.config/ranger/scope.sh": {"linux": ""},
