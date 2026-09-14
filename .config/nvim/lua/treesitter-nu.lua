3475ddf4435e40427da5a7c98d2206e31d4fc0c8
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2026-09-14 19:03:53 +0200

    nvim/treesitter-nu.lua

--- .config/nvim/lua/treesitter-nu.lua
+++ .config/nvim/lua/treesitter-nu.lua
@@ -1,7 +1,13 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "nu" },
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+require("nvim-treesitter").setup({})
+
+local skipped_filetypes = {
+    "bash",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        if not vim.tbl_contains(skipped_filetypes, vim.bo[args.buf].filetype) then
+            pcall(vim.treesitter.start, args.buf)
+        end
+    end,
 })
